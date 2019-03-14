48b87c23b73d11d47d38a1992886500b3975b9a8
wfpinedar <wfpinedar@gmail.com>
2019-03-13 23:39:40 -0500

    add model of game

--- src/chess_practice/chess_engine/open_pgn.py
+++ src/chess_practice/chess_engine/open_pgn.py
@@ -5,17 +5,21 @@ import chess.svg
 path = os.path.dirname(os.path.abspath(__file__))
 
 
-pgn = open(os.path.join(path, 'chess_practice/chess_engine/db.pgn'), encoding="ISO-8859-1")
+pgn = open(os.path.join(path, 'src/chess_practice/chess_engine/db.pgn'), encoding="ISO-8859-1")
 
+#for game in pgn:
+#    print(chess.pgn.Mainline(pgn))
 first_game = chess.pgn.read_game(pgn)
 second_game = chess.pgn.read_game(pgn)
+#
+# first_game.headers["Event"]
+# second_game.headers["Event"]
+#
+#
+# board = second_game.board()
+# squares = board.attacks(chess.F6)
+# chess.svg.board(board=board, squares=squares)
+# [head for head in second_game.headers] headers
+# [print(i) for i in first_game.variations] Variaciones.
 
-first_game.headers["Event"]
-second_game.headers["Event"]
-
-
-board = second_game.board()
-squares = board.attacks(chess.F6)
-chess.svg.board(board=board, squares=squares)
-
-print(board)
+# print(board)
--- src/chess_practice/models.py
+++ src/chess_practice/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Game(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    event = models.CharField(max_length=200)
+    site = models.CharField(max_length=200)
+    date = models.CharField(max_length=50)
+    round = models.CharField(max_length=10)
+    white = models.CharField(max_length=100)
+    black = models.CharField(max_length=100)
+    result = models.CharField(max_length=10)
+    annotator = models.CharField(max_length=200, blank=True)
+    plycount = models.CharField(max_length=200, blank=True)
+    time_control = models.CharField(max_length=200, blank=True)
+    time = models.CharField(max_length=200, blank=True)
+    termination = models.CharField(max_length=200, blank=True)
+    mode = models.CharField(max_length=200, blank=True)
+    fen = models.CharField(max_length=400, blank=True)
+    move_text = models.TextField()
+    comments = models.TextField(max_length=500, blank=True)
+
